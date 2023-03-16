b4c03e4043c4040dab827abbcbc7b50746e50f8a
Elvan Hidayat <elvanhd@gmail.com>
2023-03-17 06:31:11 +0700

    extract the router configuration

--- src/main.js
+++ src/main.js
@@ -1,23 +1,5 @@
-import ViewNotes from '@/views/ViewNotes.vue';
-import ViewStats from '@/views/ViewStats.vue';
+import router from '@/router';
 import { createApp } from 'vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'notes',
-      component: ViewNotes,
-    },
-    {
-      path: '/stats',
-      name: 'stats',
-      component: ViewStats,
-    },
-  ],
-});
 
 createApp(App).use(router).mount('#app');
