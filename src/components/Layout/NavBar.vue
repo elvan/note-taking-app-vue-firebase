aebb37492233382732e084650c977d57bb3596f2
Elvan Hidayat <elvanhd@gmail.com>
2023-03-17 06:43:41 +0700

    navbar navigation and logo

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,13 +1,7 @@
 <template>
   <nav class="navbar is-success" aria-label="main navigation" role="navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
-        />
-      </a>
+      <div class="navbar-item is-size-4 is-family-monospace">NoteTaking</div>
 
       <a
         role="button"
@@ -23,33 +17,13 @@
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item"> Home </a>
-
-        <a class="navbar-item"> Documentation </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link"> More </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item"> About </a>
-            <a class="navbar-item"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item"> Report an issue </a>
-          </div>
-        </div>
-      </div>
-
       <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
-        </div>
+        <RouterLink to="/" class="navbar-item" active-class="is-active">
+          Notes
+        </RouterLink>
+        <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+          Stats
+        </RouterLink>
       </div>
     </div>
   </nav>
