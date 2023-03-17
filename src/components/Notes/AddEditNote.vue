06d50e25479ef27012d6b003c280e86a505b90d3
Elvan Hidayat <elvanhd@gmail.com>
2023-03-17 08:49:41 +0700

    create reusable AddEditNote component

---
+++ src/components/Notes/AddEditNote.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card has-background-success-dark p-4 mb-5">
+    <div class="field">
+      <div class="control">
+        <textarea
+          :value="props.modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+          ref="newNoteRef"
+          class="textarea"
+          placeholder="Add a new note..."
+        />
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/*
+  props
+*/
+
+const props = defineProps({
+  newNote: {
+    type: String,
+    required: true,
+  },
+});
+
+/*
+  emits
+*/
+
+const emit = defineEmits(['update:modelValue']);
+</script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
-      <div class="field">
-        <div class="control">
-          <textarea
-            v-model="newNote"
-            class="textarea"
-            placeholder="Add a new note"
-            ref="newNoteRef"
-          />
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            @click="addNote"
-            :disabled="!newNote"
-            class="button is-link has-background-success"
-          >
-            Add New Note
-          </button>
-        </div>
-      </div>
-    </div>
+    <AddEditNote v-model:newNote="newNote">
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!newNote"
+          class="button is-link has-background-success"
+        >
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
 
     <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
   </div>
@@ -34,6 +21,7 @@
   imports
 */
 
+import AddEditNote from '@/components/Notes/AddEditNote.vue';
 import Note from '@/components/Notes/Note.vue';
 import { useStoreNotes } from '@/stores/storeNotes';
 import { ref } from 'vue';
