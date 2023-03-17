4d69d7649bcdd062ad50cd635319fefecafda5bc
Elvan Hidayat <elvanhd@gmail.com>
2023-03-17 13:59:53 +0700

    stats page and its getters

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -25,10 +25,20 @@
         :class="{ 'is-active': showMobileNav }"
       >
         <div class="navbar-end">
-          <RouterLink to="/" class="navbar-item" active-class="is-active">
+          <RouterLink
+            @click="showMobileNav = false"
+            to="/"
+            class="navbar-item"
+            active-class="is-active"
+          >
             Notes
           </RouterLink>
-          <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+          <RouterLink
+            @click="showMobileNav = false"
+            to="/stats"
+            class="navbar-item"
+            active-class="is-active"
+          >
             Stats
           </RouterLink>
         </div>
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -42,5 +42,15 @@ export const useStoreNotes = defineStore('storeNotes', {
         return state.notes.filter((note) => note.id === id)[0].content;
       };
     },
+    totalNotesCount: (state) => {
+      return state.notes.length;
+    },
+    totalCharactersCount: (state) => {
+      let count = 0;
+      state.notes.forEach((note) => {
+        count += note.content.length;
+      });
+      return count;
+    },
   },
 });
