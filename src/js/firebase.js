72bebe76facc82a06832c1983136299e94361bd9
Elvan Hidayat <elvanhd@gmail.com>
2023-03-18 09:31:19 +0700

    firestore authentication and auth store

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,8 +1,10 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from './firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
---
+++ src/stores/storeAuth.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+export const useStoreAuth = defineStore('storeAuth', {
+  state: () => {
+    return {};
+  },
+  actions: {},
+});
