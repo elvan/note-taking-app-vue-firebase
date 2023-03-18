0ac3903ca6d9d5dd5e18474cbcd382c17457a4cf
Elvan Hidayat <elvanhd@gmail.com>
2023-03-18 07:59:01 +0700

    display notes from firestore

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,22 @@
 */
 
 import NavBar from '@/components/Layout/NavBar.vue';
+import { useStoreNotes } from '@/stores/storeNotes';
+import { onMounted } from 'vue';
+
+/*
+  store
+*/
+
+const storeNotes = useStoreNotes();
+
+/*
+  mounted
+*/
+
+onMounted(() => {
+  storeNotes.getNotes();
+});
 </script>
 
 <style>
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,22 +1,24 @@
+import { db } from '@/js/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { defineStore } from 'pinia';
 
 export const useStoreNotes = defineStore('storeNotes', {
   state: () => {
     return {
-      notes: [
-        {
-          id: 'id1',
-          content:
-            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime.',
-        },
-        {
-          id: 'id2',
-          content: 'This is a shorter note! Woo!',
-        },
-      ],
+      notes: [],
     };
   },
   actions: {
+    async getNotes() {
+      const querySnapshot = await getDocs(collection(db, 'notes'));
+      querySnapshot.forEach((doc) => {
+        let note = {
+          id: doc.id,
+          content: doc.data().content,
+        };
+        this.notes.push(note);
+      });
+    },
     addNote(newNoteContent) {
       let currentDate = new Date().getTime(),
         id = currentDate.toString();
