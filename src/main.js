51efe4871b65fc55a602559fd83fa1812dcd5bb6
Elvan Hidayat <elvanhd@gmail.com>
2023-03-18 10:10:48 +0700

    redirect user on auth change

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import router from '@/router';
 import { createPinia } from 'pinia';
-import { createApp } from 'vue';
+import { createApp, markRaw } from 'vue';
 import App from './App.vue';
 
-createApp(App).use(createPinia()).use(router).mount('#app');
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+
+createApp(App).use(pinia).use(router).mount('#app');
